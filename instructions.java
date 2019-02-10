6c6102e5ff3dc1f73d176cc3e58a82661472a764
MSGuy01 <47047931+MSGuy01@users.noreply.github.com>
2019-02-09 16:34:20 -0800

    Add files via upload

---
+++ instructions.java
@@ -0,0 +1,7 @@
+import static java.lang.System.*;
+public class instructions extends displayer{
+    public static void readInstructions() {
+        //work on instructions!
+        out.println("Here are the instructions for Yahtzee:");
+    }
+}
